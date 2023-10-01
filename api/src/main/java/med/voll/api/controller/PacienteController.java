a8a5c5f68b91c3076a4fd4a17cc98df7422f755d
Igor Augusto <igoraugustobz@gmail.com>
2023-09-30 21:55:09 -0300

    chore: novas dependências adicionadas ao projeto

--- api/src/main/java/med/voll/api/controller/PacienteController.java
+++ api/src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,5 +1,6 @@
 package med.voll.api.controller;
 
+import med.voll.api.paciente.DadosCadastroPaciente;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
