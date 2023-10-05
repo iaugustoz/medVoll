e8a43e0abf212ef8b3f538fd6b176c9b6f7e8b5d
Igor Augusto <igoraugustobz@gmail.com>
2023-10-04 21:10:39 -0300

    feat: criação de migrations com FlyWay

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,6 +1,11 @@
 package med.voll.api.controller;
 
+import med.voll.api.endereco.Endereco;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("medicos")
 public class MedicoController {
+
+    @Autowired
+    private MedicoRepository repository;
+
     @PostMapping
+    @Transactional
     public void cadastrar(@RequestBody DadosCadastroMedico dados) {
-        System.out.println(dados);
+        repository.save(new Medico(dados));
     }
 
 }
