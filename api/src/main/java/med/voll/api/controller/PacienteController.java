3c45ddd51e60f7068de1ffae056a225fd09a991b
Igor Augusto <igoraugustobz@gmail.com>
2023-10-06 18:01:21 -0300

    feat: adicionada a funcionalidade de listagem de pacientes

--- api/src/main/java/med/voll/api/controller/PacienteController.java
+++ api/src/main/java/med/voll/api/controller/PacienteController.java
@@ -2,9 +2,13 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.paciente.DadosCadastroPaciente;
+import med.voll.api.paciente.DadosListagemPaciente;
 import med.voll.api.paciente.Paciente;
 import med.voll.api.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,4 +25,8 @@ public class PacienteController {
         repository.save(new Paciente(dados));
     }
 
+    @GetMapping
+    public Page<DadosListagemPaciente> listar(@PageableDefault(size = 10, sort = {"nome"})Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListagemPaciente::new);
+    }
 }
---
+++ api/src/main/java/med/voll/api/paciente/DadosListagemPaciente.java
@@ -0,0 +1,8 @@
+package med.voll.api.paciente;
+
+public record DadosListagemPaciente(String nome, String email, String cpf, String telefone) {
+
+    public DadosListagemPaciente(Paciente paciente) {
+        this(paciente.getNome(), paciente.getEmail(), paciente.getCpf(), paciente.getTelefone());
+    }
+}
