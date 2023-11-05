56d8c776813cca6799fd3b8552a6fd3496d0b1f6
Igor Augusto <igoraugustobz@gmail.com>
2023-11-05 17:16:37 -0300

    feat: adicionada a funcionalidade de atualização de dados dos pacientes

--- api/src/main/java/med/voll/api/paciente/DadosListagemPaciente.java
+++ api/src/main/java/med/voll/api/paciente/DadosListagemPaciente.java
@@ -1,8 +1,8 @@
 package med.voll.api.paciente;
 
-public record DadosListagemPaciente(String nome, String email, String cpf, String telefone) {
+public record DadosListagemPaciente(Long id, String nome, String email, String cpf, String telefone) {
 
     public DadosListagemPaciente(Paciente paciente) {
-        this(paciente.getNome(), paciente.getEmail(), paciente.getCpf(), paciente.getTelefone());
+        this(paciente.getId(), paciente.getNome(), paciente.getEmail(), paciente.getCpf(), paciente.getTelefone());
     }
 }
