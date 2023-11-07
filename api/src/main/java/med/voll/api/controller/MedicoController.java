5c286a762342ed092b63a167fd466c14f8fe9966
Igor Augusto <igoraugustobz@gmail.com>
2023-11-07 11:21:15 -0300

    feat: implementação do método de detalhar os dados do médico

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -54,4 +54,10 @@ public class MedicoController {
         return ResponseEntity.noContent().build();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity detalhar(@PathVariable Long id) {
+        var medico = repository.getReferenceById(id);
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
+    }
+
 }
--- api/src/main/java/med/voll/api/medico/DadosDetalhamentoMedico.java
+++ api/src/main/java/med/voll/api/medico/DadosDetalhamentoMedico.java
@@ -2,11 +2,11 @@ package med.voll.api.medico;
 
 import med.voll.api.endereco.Endereco;
 
-public record DadosDetalhamentoMedico(Long id, String nome, String crm, String telefone,
+public record DadosDetalhamentoMedico(Long id, String nome, String email,String crm, String telefone,
                                       Especialidade especialidade, Endereco endereco) {
 
     public DadosDetalhamentoMedico(Medico medico) {
-        this(medico.getId(), medico.getEmail(), medico.getCrm(), medico.getTelefone(), medico.getEspecialidade(),
+        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getTelefone(), medico.getEspecialidade(),
                 medico.getEndereco());
     }
 }
