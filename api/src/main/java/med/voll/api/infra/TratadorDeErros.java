cd1fd2849a4cfdcdf165404ca1fb12961bb00518
Igor Augusto <igoraugustobz@gmail.com>
2023-11-30 19:14:28 -0300

    feat: adicionada a entidade Usuario

--- api/src/main/java/med/voll/api/infra/TratadorDeErros.java
+++ api/src/main/java/med/voll/api/infra/TratadorDeErros.java
@@ -10,13 +10,11 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class TratadorDeErros {
 
-    // Retorna um erro 404 caso haja uma exception nesse estilo
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity tratarErro404() {
         return ResponseEntity.notFound().build();
     }
 
-    //
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity tratarErro400(MethodArgumentNotValidException e) {
         var errors = e.getFieldErrors();
