42eba5557de04eec8b094369b3cdb4e4d3ff3e0e
Igor Augusto <igoraugustobz@gmail.com>
2023-12-02 19:59:50 -0300

    feat: adicionada a funcionalidade de validação do token

--- api/src/main/java/med/voll/api/domain/usuario/Usuario.java
+++ api/src/main/java/med/voll/api/domain/usuario/Usuario.java
@@ -26,7 +26,6 @@ public class Usuario implements UserDetails {
     private String login;
     private String password;
 
-
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of(new SimpleGrantedAuthority("ROLE_USER"));
