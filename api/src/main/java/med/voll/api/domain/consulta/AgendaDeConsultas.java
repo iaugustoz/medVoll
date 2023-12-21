33bf1d20d4fbb587b473faa1b5144cdfaed75805
Igor Augusto <igoraugustobz@gmail.com>
2023-12-21 20:22:17 -0300

    feature: implementada a funcionalidade de cancelar consultas

--- api/src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
+++ api/src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -31,8 +31,7 @@ public class AgendaDeConsultas {
 
         var medico = escolherMedico(dados);
         var paciente = pacienteRepository.getReferenceById(dados.idPaciente());
-        var consulta = new Consulta(null, medico, paciente, dados.data());
-
+        var consulta = new Consulta(null, medico, paciente, dados.data(), null);
         consultaRepository.save(consulta);
     }
 
@@ -46,5 +45,14 @@ public class AgendaDeConsultas {
         }
 
         return medicoRepository.escolherMedicoAleatorioLivreNaData(dados.especialidade(), dados.data());
+    }
+
+    public void cancelar(DadosCancelamentoConsulta dados) {
+        if (!consultaRepository.existsById(dados.idConsulta())) {
+            throw new ValidacaoException("Id da consulta informada não existe!");
+        }
+
+        var consulta = consultaRepository.getReferenceById(dados.idConsulta());
+        consulta.cancelar(dados.motivo());
     }
 }
